e07936ecb60967e7c164e16b85a37364cc14e8cf
Manoel Tindão <timdao02@hotmail.com>
2020-05-29 15:07:37 -0300

    Criação do projeto

---
+++ src/main/java/com/tindao/cursoespecialistaspringrest01/CursoEspecialistaSpringRest01Application.java
@@ -0,0 +1,13 @@
+package com.tindao.cursoespecialistaspringrest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoEspecialistaSpringRest01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoEspecialistaSpringRest01Application.class, args);
+	}
+
+}
